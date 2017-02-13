1a0429e084f2625fd56052af33ae0dab947e5bd6
ultimatum424 <Good20032014>
2017-02-14 02:03:03 +0300

    CBankClient is defined by the application settings Add Help

--- PP2/PP2.cpp
+++ PP2/PP2.cpp
@@ -1,18 +1,85 @@
 #include "stdafx.h"
 #include "Bank.h"
 #include "BankClient.h"
+#include <string>
 
-
-int main()
+namespace
 {
-	CBank* bank = new CBank(2, SyncPrimitivesEnum::Mutex);
-	
-
-	// fixed TODO: WaitForMultipleObjects
-	while (true)
+	void PrintHelpForStart()
 	{
-		bank->WaitForClients();
+		std::cout << "Using command string : PP2.exe <count clients> <type SyncPrimitive>" << std::endl;
+		std::cout << "Code for using  SyncPrimitive: " << std::endl;
+		std::cout << "1 - critical section" << std::endl;
+		std::cout << "2 - mutex" << std::endl;
+		std::cout << "3 - semafor" << std::endl;
+		std::cout << "4 - event" << std::endl;
+		std::cout << "? - for Help" << std::endl;
 	}
 
-    return 0;
+	SyncPrimitivesEnum FindSyncPrimitiveFormArgv(std::string param)
+	{
+		if (param == "1")
+		{
+			return SyncPrimitivesEnum::CriticalSection;
+		}
+		else if (param == "2")
+		{
+			return SyncPrimitivesEnum::Mutex;
+		}
+		else if (param == "3")
+		{
+			return SyncPrimitivesEnum::Semaphore;
+		}
+		else if (param == "4")
+		{
+			return SyncPrimitivesEnum::Event;
+		}
+		return SyncPrimitivesEnum::None;
+	}
+}
+
+
+int main(int argc, char* argv[])
+{
+	PrintHelpForStart();
+
+	if (argc > 1)
+	{
+		if (argv[1] == "/?")
+		{
+			PrintHelpForStart();
+		}
+		else
+		{
+			CBank* bank;
+			unsigned cliensCount;
+			// fixed TODO: WaitForMultipleObjects
+			try
+			{
+				cliensCount = std::stoi(argv[1]);
+			}
+			catch (std::exception)
+			{
+				std::cout << "Invalid argument" << std::endl;
+				return 1;
+			}
+			if (argc == 3)
+			{
+				bank = new CBank(cliensCount, FindSyncPrimitiveFormArgv(argv[2]));
+			}
+
+			else
+			{
+				bank = new CBank(cliensCount, SyncPrimitivesEnum::None);
+			}
+
+			while (true)
+			{
+				bank->WaitForClients();
+			}
+		}
+	}
+	
+	return 0;
+		
 }
